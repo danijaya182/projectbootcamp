f9c953edf0a7a9600dc1372343d715f4aef95508
dani <danieltjahjadi88@gmail.com>
2019-09-01 17:02:06 +0700

    komplit crud dan API

---
+++ controllers/notes.js
@@ -0,0 +1,91 @@
+const noteModel = require('../models/notes');
+
+module.exports = {
+    getById: (req, res, next) => {
+        noteModel.findById(req.params.noteId, (err, noteInfo)=>{ 
+        if (err){
+            next (err);
+        } else {
+            res.json({
+                status: "success",
+                message: "ok",
+                date: {
+                    note: noteInfo
+                }
+            });
+        }
+        });
+    },
+    getAll: (req, res, next) => {
+        let  noteList = [];
+        noteModel.find({}, (err,notes) => {
+            if (err) {
+                next (err);
+            } else {
+                for (let note of notes){
+                    noteList.push({
+                        id: note._id,
+                        name: note.name,
+                        
+                    });
+                }
+                res.json({
+                    status: "success",
+                    message: "ok",
+                    date: {
+                        note: noteList
+                    }
+                });
+
+            }
+        });
+    },
+    updateById: (req, res, next) => {
+        noteModel.findByIdAndUpdate(req.params.noteId,
+        { 
+            name: req.body.name,
+            content : req.body.content
+        },
+        (err, noteInfo) => {
+            if (err) {
+                next(err);
+            } else {
+                res.json({
+                    status: "success",
+                    message: "ok",
+                    data: null
+                });
+            }
+        })
+    },
+    deleteById: (req, res, next) => {
+        noteModel.findByIdAndRemove(req.params.noteId, (err, noteInfo) => {
+            if (err) {
+                next(err);
+            } else {
+                res.json({
+                    status: "success",
+                    message: "ok",
+                    data: null
+                });
+            }
+        });
+    },
+    create: (req, res, next) => {
+        noteModel.create({
+            name: req.body.name,
+            content: req.body.content,
+            userId: req.body.userId,
+        }, (err, result) => {
+            if (err) {
+                next(err);
+            } else { 
+                res.json({
+                   status: "success",
+                   message: "ok",
+                   data: null
+               });
+           }
+        })
+    }
+}
--- server.js
+++ server.js
@@ -1,14 +1,66 @@
 const express = require ('express');
 const logger = require ('morgan');
 const bodyParser = require ('body-parser');
-const app = express ();
+const app = express();
+
+
+//
+const users = require ('./routes/users');
+const notes = require ('./routes/notes');
+
+const mongoose = require ('./config/database');
+var jwt = require ('jsonwebtoken');
+app.use(bodyParser.urlencoded({extended: false}));
+app.set('jwtSecretKey', 'kunciRahasia');
+
+//
+mongoose.connection.on('error',
+console.error.bind(console, 'MongoDB connection error:')
+);
+
+//
 app.use (logger('dev'));
 app.use(bodyParser.urlencoded({extended:false}));
 
 app.get('/', (req, res) => {
-    res.json({"message": "Dani sang Maestro Developer"});
+    res.json({"message": "hello world"});
 });
 
+//
+app.use('/users', users);
+
+//
+app.use('/notes', validateUser, notes);
+
+//
+function validateUser(req, res, next) {
+  jwt.verify(req.headers['x-access-token'], req.app.get('jwtSecretKey'), (err, decoded) => {
+      if (err) {
+        res.status(401).json({status:"error", message : "unauthorized", data:null});
+      } else {
+        req.body.userId = decoded.id;
+        next();
+      }
+    });
+}
+    
+
+//
+app.use('/users', users);
+app.use((req, res, next)=>{
+ let err = new Error('Not Found');
+ err.status = 404;
+ next(err);
+});
+app.use((err, req, res, next) => {
+    console.log(err);
+    if(err.status === 404) {
+       res.status(404).json({message: "Not Found"});
+    } else {
+       res.status(500).json({message:"Server error"});
+    }
+});    
+ 
 app.listen(3000, () => {
     console.log('Node server listening on port 3000');
 });
