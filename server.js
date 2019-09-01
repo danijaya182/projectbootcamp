341f14534cffe14f00ffc3bdbbe3668fb7bd7667
dani <danieltjahjadi88@gmail.com>
2019-09-01 10:43:25 +0700

    materi hari minggu

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require ('express');
+const logger = require ('morgan');
+const bodyParser = require ('body-parser');
+const app = express ();
+app.use (logger('dev'));
+app.use(bodyParser.urlencoded({extended:false}));
+
+app.get('/', (req, res) => {
+    res.json({"message": "Dani sang Maestro Developer"});
+});
+
+app.listen(3000, () => {
+    console.log('Node server listening on port 3000');
+});
